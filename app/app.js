0ad845a13802d1bf607e58fff0245077bce6d6d5
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-03-20 14:43:54 -0400

    Initial Commit

---
+++ app/app.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name trakr
+   * @requires $urlRouterProvider
+   *
+   * @description
+   *
+   */
+  angular
+    .module('trakr', [
+      'ui.router',
+      'mgcrea.ngStrap',
+      'home'
+    ]);
+
+  angular
+    .module('trakr')
+    .config(config);
+
+  function config($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+  }
+
+})();
